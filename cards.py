61049890ef189c473fdc66cbbf42cf1f7a1a258f
Chris Allen <tekkie1.618@gmail.com>
2021-04-04 09:55:50 -0400

    working

--- cards.py
+++
@@ -1,6 +0,0 @@
-import random, itertools
-
-cards = list(itertools.product(range(1, 14), ["spades", "diamonds", "clubs", "hearts"]))
-random.shuffle(cards)
-for n in range(52):
-    print(f"{cards[n][0]} of {cards[n][1]}")
