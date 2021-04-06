d9e793450742d5c4e7ec52db38731b96530c3936
Chris Allen <tekkie1.618@gmail.com>
2021-04-06 17:47:21 -0400

    working

--- random_base.py
+++ random_base.py
@@ -1,13 +1,17 @@
 from random import randint
+from string import ascii_uppercase
 
 
 def random_base(n, b):
     rand_int = ""
+    alpha = ascii_uppercase
     for num in range(n):
-        del num
+        if num >= 10:
+            num = alpha[num - 9]
+            print(alpha[num - 9])
         rand_int = f"{rand_int}{randint(0, b - 1)}"
 
     return rand_int.zfill(n)
 
 
-print(random_base(5, 2))
+print(random_base(5, 12))
